5bbf1e9fae6ea4ae9d95f6de29d2fcaf4ac52389
tiago1994 <tiagovolta@gmail.com>
2015-11-17 07:26:53 -0200

    Quase terminando

--- ci1/application/controllers/produtos.php
+++ ci1/application/controllers/produtos.php
@@ -1,14 +1,12 @@
-<?
-	class Produtos extends CI_Controller{
-		public function index(){
-			$this->load->database();
-			$this->load->model("produtos_model");
-			$produtos = $this->produtos_model->buscatodos();
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+class Produtos extends CI_Controller{
+	public function index(){
+		$this->load->model("produtos_model");
+		$produtos = $this->produtos_model->buscatodos();
 
-			$dados = array("produtos" => $produtos);
-			$this->load->helper("url");
-			$this->load->helper("currency");
-			$this->load->view("produtos/index.php", $dados);
-		}
+		$dados = array("produtos" => $produtos);
+		$this->load->helper(array("url", "currency", "form"));
+		$this->load->view("produtos/index.php", $dados);
 	}
+}
 ?>
